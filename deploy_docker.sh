6010d873a1e4a91ce5ad89fbf94106db620454bb
ninjadev999 <mikenarkss@gmail.com>
2019-03-29 07:52:38 -0700

    updated deployment script

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -6,8 +6,8 @@ docker-compose stop
 
 # Re-build npm on local
 nvm use 11.2.0
-npm install
-npm run build
+yarn install
+yarn run build
 
 # Re-build docker container
 docker-compose build --no-cache
